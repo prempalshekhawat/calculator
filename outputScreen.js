733da767e486736f54a49e698b9630842aa683f6
Prempal Shekhawat <77876294+prempalshekhawat@users.noreply.github.com>
2021-02-22 12:09:29 +0530

    Add files via upload

---
+++ outputScreen.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import OutputScreenRow from './outputScreenRow.js';
+const OutputScreen =(props)=> {
+    return (
+        <div className="screen"> 
+        <OutputScreenRow value = {props.question}/>
+            <OutputScreenRow value = {props.answer}/>
+                </div>
+    )
+}
+export default OutputScreen;
+
